6fbef63573d268e85f80c7a895b280a15a6e297b
Damien Vesper <ldamienvesper@gmail.com>
2023-10-01 08:07:22 -0500

    push settings for get

--- server/src/routes/settings/Get.ts
+++ server/src/routes/settings/Get.ts
@@ -7,9 +7,7 @@ router.get(`/`, (req, res) => {
     req.isAuthenticated()
         ? res.status(200).json({
             authenticated: true,
-            username: (req.user as any).username,
-            displayName: (req.user as any).displayName,
-            avatar: (req.user as any).avatar
+            settings: (req.user as any).settings
         })
         : res.status(200).json({ authenticated: false });
 });
