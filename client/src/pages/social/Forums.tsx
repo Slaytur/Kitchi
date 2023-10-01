3e552f5a1c82091172e71105ad75997e2f1d6929
Damien Vesper <ldamienvesper@gmail.com>
2023-10-01 10:09:51 -0500

    NUKE THE FORUM SCSSS

--- client/src/pages/social/Forums.tsx
+++ client/src/pages/social/Forums.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../assets/scss/pages/forums.scss";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane, faPlus } from "@fortawesome/free-solid-svg-icons";
 
