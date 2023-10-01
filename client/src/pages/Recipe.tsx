5e66053d387a093b82e87554d898dd85811da068
smite <kjsmith415@gmail.com>
2023-10-01 11:22:21 -0500

    oh my god

--- client/src/pages/Recipe.tsx
+++ client/src/pages/Recipe.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import '../assets/scss/pages/recipe.scss';
+import "../assets/scss/pages/recipe.scss";
 
 declare const API_URL: string;
 
@@ -9,18 +9,83 @@ declare const API_URL: string;
  * The 404 page.
  */
 class Recipe extends React.Component {
-    render = (): React.ReactNode => {
-        return (
-            <main className="text-center tw-mb-5">
-                Hello
-            </main>
-        );
-    };
+    render = (): React.ReactNode => (
+        <main className="tw-text-center tw-px-[10%] tw-w-screen tw-h-fit tw-bg-[#FFFFFF]">
+            <div className="tw-h-full tw-w-full tw-bg-white tw-p-8 tw-text-left">
+                <h1 className="tw-w-full tw-font-bold tw-mt-10 tw-pl-1 tw-text-[40px]">
+                    Community-Backed Recipes
+                </h1>
+                <div className="tw-h-[2px] tw-w-full -tw-mt-2 tw-mb-3 tw-mx-2 tw-opacity-30 tw-bg-offblack"></div>
+
+                <div className="tw-grid tw-gap-4 tw-overflow-auto tw-grid-cols-2 tw-sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
+                    {/* Recipe Card 1 */}
+                    <div className="card tw-p-3 tw-flow-root">
+                        <h1 className="tw-text-[24px]">Recipe Card</h1>
+                        <p className="tw-mb-60">Description</p>
+                        <button className="tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                            Add to Cookbook
+                        </button>
+                    </div>
+
+                    {/* Recipe Card 2 */}
+                    <div className="card tw-p-3 tw-flow-root">
+                        <h1 className="tw-text-[24px]">Recipe Card</h1>
+                        <p className="tw-mb-60">Description</p>
+                        <button className="tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                            Add to Cookbook
+                        </button>
+                    </div>
+
+                    {/* Repeat the above card structure for a total of 6 cards */}
+                    {/* Recipe Card 3 */}
+                    <div className="card tw-p-3 tw-flow-root">
+                        <h1 className="tw-text-[24px]">Recipe Card</h1>
+                        <p className="tw-mb-60">Description</p>
+                        <button className="tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                            Add to Cookbook
+                        </button>
+                    </div>
+
+                    {/* Recipe Card 4 */}
+                    <div className="card tw-p-3 tw-flow-root">
+                        <h1 className="tw-text-[24px]">Recipe Card</h1>
+                        <p className="tw-mb-60">Description</p>
+                        <button className="tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                            Add to Cookbook
+                        </button>
+                    </div>
+
+                    {/* Recipe Card 5 */}
+                    <div className="card tw-p-3 tw-flow-root">
+                        <h1 className="tw-text-[24px]">Recipe Card</h1>
+                        <p className="tw-mb-60">Description</p>
+                        <button className="tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                            Add to Cookbook
+                        </button>
+                    </div>
+
+                    {/* Recipe Card 6 */}
+                    <div className="card tw-p-3 tw-flow-root">
+                        <h1 className="tw-text-[24px]">Recipe Card</h1>
+                        <p className="tw-mb-60">Description</p>
+                        <button className="tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                            Add to Cookbook
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 
     componentDidMount = (): void => {
-        void axios.get(`${API_URL}/api/${window.location.pathname}`).then(res => {
-            return res.data;
-        }).catch(() => console.error(`[ACCOUNT SERVER]: Could not get recipe.`));
+        void axios
+            .get(`${API_URL}/api/${window.location.pathname}`)
+            .then((res) => {
+                return res.data;
+            })
+            .catch(() =>
+                console.error(`[ACCOUNT SERVER]: Could not get recipe.`)
+            );
     };
 }
 
--- client/src/pages/account/Dashboard.tsx
+++ client/src/pages/account/Dashboard.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
 import "../../assets/scss/pages/dashboard.scss";
 // import { PassThrough } from "stream";
@@ -12,41 +12,54 @@ class Dashboard extends React.Component {
         <main className="tw-text-center">
             <div className="tw-px-[10%] tw-h-fit tw-bg-[#ffffff]">
                 <div className="tw-h-full tw-w-full tw-bg-white tw-p-8 tw-text-left">
-                    <h1 className="tw-w-full tw-font-bold tw-mt-10 tw-pl-1 tw-text-[40px]">Recommended</h1>
+                    <h1 className="tw-w-full tw-font-bold tw-mt-10 tw-pl-1 tw-text-[40px]">
+                        Recommended
+                    </h1>
                     <div className="tw-h-[2px] tw-w-full -tw-mt-2 tw-mb-3 tw-mx-2 tw-opacity-30 tw-bg-offblack"></div>
 
                     <div className="tw-grid tw-gap-4 tw-grid-flow-col-dense tw-overflow-auto tw-auto-cols-max tw-sm:grid-flow-row">
                         <div className="card tw-p-3 tw-flow-root">
                             <h1 className="tw-text-[24px]">Recipe Card</h1>
                             <p className=" tw-mb-60"> Description </p>
-                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">Add to Cookbook</button>
+                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                                Add to Cookbook
+                            </button>
                         </div>
                         <div className="card tw-p-3 tw-flow-root">
                             <h1 className="tw-text-[24px]">Recipe Card</h1>
                             <p className=" tw-mb-60"> Description </p>
-                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">Add to Cookbook</button>
+                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                                Add to Cookbook
+                            </button>
                         </div>
                         <div className="card tw-p-3 tw-flow-root">
                             <h1 className="tw-text-[24px]">Recipe Card</h1>
                             <p className=" tw-mb-60"> Description </p>
-                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">Add to Cookbook</button>
+                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                                Add to Cookbook
+                            </button>
                         </div>
                         <div className="card tw-p-3 tw-flow-root">
                             <h1 className="tw-text-[24px]">Recipe Card</h1>
                             <p className=" tw-mb-60"> Description </p>
-                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">Add to Cookbook</button>
+                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                                Add to Cookbook
+                            </button>
                         </div>
                         <div className="card tw-p-3 tw-flow-root">
                             <h1 className="tw-text-[24px]">Recipe Card</h1>
                             <p className=" tw-mb-60"> Description </p>
-                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">Add to Cookbook</button>
+                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                                Add to Cookbook
+                            </button>
                         </div>
                         <div className="card tw-p-3 tw-flow-root">
                             <h1 className="tw-text-[24px]">Recipe Card</h1>
                             <p className=" tw-mb-60"> Description </p>
-                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">Add to Cookbook</button>
+                            <button className=" tw-w-fit tw-px-2 tw-bg-offwhite tw-py-1 tw-rounded-full tw-float-right">
+                                Add to Cookbook
+                            </button>
                         </div>
-
                     </div>
 
                     {/* <div className="tw-w-full tw-p-2 tw-justify-center tw-flex">
@@ -58,7 +71,9 @@ class Dashboard extends React.Component {
 
                     {/* cookbook */}
 
-                    <h1 className="tw-w-full tw-font-bold tw-mt-10 tw-pl-1 tw-text-[40px]">Cookbook</h1>
+                    <h1 className="tw-w-full tw-font-bold tw-mt-10 tw-pl-1 tw-text-[40px]">
+                        Cookbook
+                    </h1>
                     <div className=" tw-grid-cols-1 tw-grid shadow tw-max-h-48 tw-gap-y-2 tw-p-4">
                         <button className=" tw-flex tw-w-full tw-rounded-full tw-align-middle  tw-px-8 tw-border-none tw-border-[2px] tw-border-[#2828289d] ">
                             <p className=" tw-font-bold my-auto">Recipe Name</p>
@@ -80,7 +95,9 @@ class Dashboard extends React.Component {
 
                     {/* Pantry */}
 
-                    <h1 className="tw-w-full tw-font-bold tw-mt-10 tw-pl-1 tw-text-[40px]">Pantry</h1>
+                    <h1 className="tw-w-full tw-font-bold tw-mt-10 tw-pl-1 tw-text-[40px]">
+                        Pantry
+                    </h1>
                     <div className="tw-flex tw-w-full">
                         <div className=" tw-w-full tw-flex tw-flex-wrap tw-h-min tw-whitespace-nowrap tw-overflow-y-scroll tw-scroll-smooth shadow tw-py-2 tw-max-h-48 tw-gap-y-2">
                             <p className="ingredient"> - Ingredient</p>
@@ -104,12 +121,18 @@ class Dashboard extends React.Component {
                             <p className="ingredient"> - Ingredient</p>
                         </div>
                         <div className=" tw-p-4">
-                            <p className="tw-w-full tw-text-center tw-mr-3 tw-text-xl">Add</p>
+                            <p className="tw-w-full tw-text-center tw-mr-3 tw-text-xl">
+                                Add
+                            </p>
                             <input type="text"></input>
                             <div className="tw-flex tw-gap-x-2 tw-mx-auto tw-my-3 tw-whitespace-nowrap tw-w-full">
-                                <button className="tw-p-1 tw-rounded-xl tw-bg-offwhite tw-w-full">Add</button>
+                                <button className="tw-p-1 tw-rounded-xl tw-bg-offwhite tw-w-full">
+                                    Add
+                                </button>
                                 <div className="tw-w-full tw-h-fit"></div>
-                                <button className="tw-p-1 tw-rounded-xl tw-bg-offwhite ">Upload Image</button>
+                                <button className="tw-p-1 tw-rounded-xl tw-bg-offwhite ">
+                                    Upload Image
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -119,14 +142,27 @@ class Dashboard extends React.Component {
     );
 
     componentDidMount = async (): Promise<void> => {
-        void axios.get(`${API_URL}/auth/authenticated`, { withCredentials: true }).then(res => {
-            if (res.data?.authenticated !== true) window.location.href = `${window.location.protocol}//${window.location.host}/auth/login`;
-        }).catch(() => console.error(`[ACCOUNT SERVER]: Could not determine login status.`));
+        void axios
+            .get(`${API_URL}/auth/authenticated`, { withCredentials: true })
+            .then((res) => {
+                if (res.data?.authenticated !== true)
+                    window.location.href = `${window.location.protocol}//${window.location.host}/auth/login`;
+            })
+            .catch(() =>
+                console.error(
+                    `[ACCOUNT SERVER]: Could not determine login status.`
+                )
+            );
 
-        void axios.get(`${API_URL}/dashboarddata`, { withCredentials: true }).then(res => {
-            if (res.data?.authenticated !== true) return;
-            return 5;
-        }).catch(() => console.error(`[ACCOUNT SERVER]: Could not get account data.`));
+        void axios
+            .get(`${API_URL}/dashboarddata`, { withCredentials: true })
+            .then((res) => {
+                if (res.data?.authenticated !== true) return;
+                return 5;
+            })
+            .catch(() =>
+                console.error(`[ACCOUNT SERVER]: Could not get account data.`)
+            );
     };
 }
 
