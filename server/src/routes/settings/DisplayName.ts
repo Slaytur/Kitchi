b8623434aa1a6848dd341f104f7d269e879d3d0c
Damien Vesper <ldamienvesper@gmail.com>
2023-10-01 05:02:01 -0500

    feat: displayname

---
+++ server/src/routes/settings/DisplayName.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { User } from '../../models/user.model';
+
+const router = Router();
+
+router.post(`/`, (req, res) => {
+    if (!req.isAuthenticated()) return res.redirect(`/login`);
+
+    if (!req.body[`display-name`] || typeof req.body[`display-name`] !== `string`) return res.json({ errors: `Please fill out all fields` });
+    if (req.body[`display-name`].length > 20) return res.json({ errors: `Your display cannot be over 20 characters` });
+    if (req.body[`display-name`].toLowerCase() !== (<any>req).user.username) return res.json({ errors: `Your display name must match your username, in spelling` });
+
+    void User.findOne({ username: (<any>req).user.username }).then(user => {
+        if (user === null) return res.json({ errors: `Invalid account data` });
+        user.displayName = req.body[`display-name`];
+
+        void user.save()
+            .then(() => res.json({ success: `Succesfully updated display name.` }))
+            .catch(() => res.json({ errors: `Invalid account data` }));
+    });
+});
+
+export default router;
