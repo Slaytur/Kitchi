9c09aeb9894dcdbfbbbf8b8a48718c79c6606760
Iakapro <michaelandbrother@gmail.com>
2023-10-01 04:39:59 -0500

    nt

--- client/src/pages/social/Forums.tsx
+++ client/src/pages/social/Forums.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import "../../assets/scss/pages/register.scss";
+import "../../assets/scss/pages/forums.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import Heart from "../../assets/img/icons/heart.png";
+import Like from "../../assets/img/icons/cookin.png";
 
 // import "client/src/assets/scss/pages/forums.scss";
 declare const API_URL: string;
 /**
  * The register page.
  */
-class Forum extends React.Component<
-Record<string, never>,
-{ isLoggedIn: boolean }
-> {
+class Forum extends React.Component<Record<string, never>, { isLoggedIn: boolean }> {
     render = (): React.ReactNode => {
         return (
             <main className="tw-text-center forum">
@@ -24,96 +22,79 @@ Record<string, never>,
                             action={`${API_URL}/forum/post`}
                             className="tw-bg-lightgreen tw-p-4 tw-w-11/12 form-control tw-mx-auto tw-border-[#58991F] tw-border-2"
                         >
+                            <label
+                                htmlFor="create-post"
+                                className="post-label tw-mx-auto"
+                            >
+                                <h2>Talk To The World</h2>
+                            </label>
                             <div className="tw-mb-3">
-                                <label
-                                    htmlFor="create-post"
-                                    className="post-label tw-mx-auto"
-                                >
-                                    <h2>Talk To The World</h2>
-                                </label>
                                 <br></br>
                                 <textarea
                                     name="post-message"
                                     id="post-email"
-                                    className="post-control tw-w-11/12 tw-mx-auto tw-resize-none tw-h-36"
+                                    className="post-control tw-p-5 tw-w-[98%] tw-mx-auto tw-bg-[#e8f3df] tw-border-[#9b9b9b] tw-resize-none tw-h-36"
                                     placeholder="Type your message here!"
                                     required
                                 />
-                                <a
-                                    href="/forum/post"
-                                    className="btn btn-lg btn-green btn-sign-in tw-p-1 tw-ms-1"
-                                >
-                                    <FontAwesomeIcon icon={faSignInAlt} />
-                                </a>
+                                <div className="tw-w-[98%] tw-bg-[#ecf5e5] tw-py-1 tw-h-11 tw-border-[1px] tw-border-[#9b9b9b]  tw-border-solid tw-mx-auto">
+                                    <a
+                                        title="addimg"
+                                        href=""
+                                        className="btn btn-lg btn-green btn-sign-in tw-py-0 tw-cursor-pointer tw-float-left tw-px-[5px] tw-ml-2"
+                                    >
+                                        <FontAwesomeIcon className="" icon={faPlus} />
+                                        <label className="tw-text-[1rem] tw-inline"> image</label>
+                                    </a>
+                                    <a
+                                        title="signin"
+                                        href="/forum/post"
+                                        className="btn btn-lg btn-green btn-sign-in tw-py-0 tw-cursor-pointer tw-float-right tw-px-1 tw-mr-3"
+                                    >
+                                        <FontAwesomeIcon className="" icon={faPaperPlane} />
+                                    </a>
+                                </div>
                             </div>
                         </form>
 
-                        <div className="past-comments tw-p-15 tw-mt-20 tw-w-11/12 tw-mx-auto tw-border-solid tw-rounded-md tw-border-[#4f4f4f94]">
-                            <div className="comment tw-bg-white tw-border-t-2 tw-border-b-0 tw-border-r-0 tw-border-solid tw-border-[#32690a62]">
-                                <img
-                                    className="tw-float-left tw-pl-5 tw-pt-1"
-                                    src={Heart}
-                                    style={{ width: `60px`, height: `50px` }}
-                                ></img>
-                                <label
-                                    htmlFor="username"
-                                    className="post-label tw-float-left tw-pl-2"
-                                >
-                                    placeholdname
-                                </label>
-                                <br></br>
-                                <label
-                                    htmlFor="message"
-                                    className="post-label tw-float-left tw-pl-3"
-                                >
-                                    get message from server
-                                </label>
-                                <br></br>
-                                <img
-                                    className="tw-float-left tw-pl-5 tw-pt-1"
-                                    src={Heart}
-                                    style={{ width: `60px`, height: `50px` }}
-                                ></img>
-                                <label
-                                    htmlFor="likes"
-                                    className="post-label tw-float-left tw-pl-3"
-                                >
-                                    likes
-                                </label>
-                                <br></br>
+                        <div className="past-comments tw-p-15 tw-grid tw-auto-rows-auto tw-mt-20 tw-w-11/12 tw-mx-auto tw-border-solid tw-rounded-md tw-border-[#4f4f4f94]">
+                            <div className="comment">
+                                <div>
+                                    <img title="pfp" className="pfp" src={Heart}></img>
+                                    <label htmlFor="username" className="username">
+                                        placeholdname
+                                    </label>
+                                </div>
+                                <div>
+                                    <label htmlFor="message" className="message">
+                                        get message from server
+                                    </label>
+                                </div>
+                                <div>
+                                    <img title="likes" className="likes" src={Like}></img>
+                                    <label htmlFor="likes" className="likesl">
+                                        likes
+                                    </label>
+                                </div>
                             </div>
-                            <div className="comment tw-bg-white tw-border-t-2 tw-border-b-0 tw-border-r-0 tw-border-solid tw-border-[#32690a62]">
-                                <img
-                                    className="tw-float-left tw-pl-5 tw-pt-1"
-                                    src={Heart}
-                                    style={{ width: `60px`, height: `50px` }}
-                                ></img>
-                                <label
-                                    htmlFor="username"
-                                    className="post-label tw-float-left tw-pl-2"
-                                >
-                                    placeholdname
-                                </label>
-                                <br></br>
-                                <label
-                                    htmlFor="message"
-                                    className="post-label tw-float-left tw-pl-3"
-                                >
-                                    get message from server
-                                </label>
-                                <br></br>
-                                <img
-                                    className="tw-float-left tw-pl-5 tw-pt-1"
-                                    src={Heart}
-                                    style={{ width: `60x`, height: `50px` }}
-                                ></img>
-                                <label
-                                    htmlFor="likes"
-                                    className="post-label tw-float-left tw-pl-3"
-                                >
-                                    likes
-                                </label>
-                                <br></br>
+                            <div className="comment">
+                                <div>
+                                    <img title="pfp" className="pfp" src={Heart}></img>
+                                    <label htmlFor="username" className="username">
+                                        placeholdname
+                                    </label>
+                                </div>
+                                <div>
+                                    <label htmlFor="message" className="message">
+                                        get message from server
+                                    </label>
+                                </div>
+                                <div>
+                                    <img title="likes" className="likes" src={Like}></img>
+                                    <label htmlFor="likes" className="likesl">
+                                        likes
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
